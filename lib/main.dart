d62ff612edbc4fd81ae9dcb3e020ca18752038c8
Fernando Fazio Sinigaglia <fernandosini@MacBook-Air-de-Fernando.local>
2022-10-23 16:46:25 -0300

    subindo codigo

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:camera/camera.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flemis/mobile/controller/default.dart';
-import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
